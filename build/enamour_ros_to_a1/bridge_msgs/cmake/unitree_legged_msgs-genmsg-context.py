faedae4f1e58904c920c1c28c759c861e196610d
Michael Angerer <mia6329@thi.de>
2022-04-09 15:26:06 +0200

    changed file structure after pull, still doesnt build

---
+++ build/enamour_ros_to_a1/bridge_msgs/cmake/unitree_legged_msgs-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/MotorCmd.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/MotorState.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/Cartesian.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/IMU.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/LED.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/LowCmd.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/LowState.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/HighCmd.msg;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg/HighState.msg"
+services_str = ""
+pkg_name = "unitree_legged_msgs"
+dependencies_str = "std_msgs;geometry_msgs;sensor_msgs"
+langs = "gencpp;geneus;genlisp;gennodejs;genpy"
+dep_include_paths_str = "unitree_legged_msgs;/home/bichl/Enamour/spike/src/enamour_ros_to_a1/bridge_msgs/msg;std_msgs;/opt/ros/noetic/share/std_msgs/cmake/../msg;geometry_msgs;/opt/ros/noetic/share/geometry_msgs/cmake/../msg;sensor_msgs;/opt/ros/noetic/share/sensor_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python3"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "/opt/ros/noetic/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
