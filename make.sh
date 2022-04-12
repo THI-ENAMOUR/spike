bf7da8b3f8c1cba5b354af85c18e9576cec396cd
sit7432 <sit7432@thi.de>
2022-04-12 14:57:51 +0200

    Add basic structure with ros and champ

---
+++ make.sh
@@ -0,0 +1,12 @@
+source /opt/ros/noetic/setup.bash
+
+catkin_make
+
+if [[ -z "${PROJECT_SCRIPTS_DIR}" ]]; then
+  ENVIRONMENT_SCRIPT="scripts/setup_environment.sh"
+else
+  ENVIRONMENT_SCRIPT="${PROJECT_SCRIPTS_DIR}/setup_environment.sh"
+fi
+
+
+source $ENVIRONMENT_SCRIPT
