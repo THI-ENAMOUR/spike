e6c8cbb29f5a5479b4aab6bf9f398dae8315ca3d
Lint Action <lint-action@samuelmeuli.com>
2022-04-12 09:28:36 +0000

    Fix code style issues with Black

--- src/dummy_node/dummy_main.py
+++ src/dummy_node/dummy_main.py
@@ -21,15 +21,15 @@ def received_message(string):
     rospy.loginfo(rospy.get_caller_id() + "Received message: ", string.data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        rospy.init_node('dummy')
+        rospy.init_node("dummy")
         init_sub()
 
-
-
-        loop_rate = rospy.get_param("loop_rate", default=10)  # Use tick from parameter server
-        timer = rospy.Timer(rospy.Duration(1/loop_rate), pub_message)
+        loop_rate = rospy.get_param(
+            "loop_rate", default=10
+        )  # Use tick from parameter server
+        timer = rospy.Timer(rospy.Duration(1 / loop_rate), pub_message)
 
         rospy.spin()
         timer.shutdown()
