2ef259082622ac844dbb5c861888c3cb34b57b05
mig3177 <mig3177@thi.de>
2022-04-25 20:13:05 +0200

    Fix launch file

---
+++ src/enamour/frontend/src/server/start_server.bash
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo ---------------------------
+echo "$PWD" 
+cd /home/lars/Desktop/project/src/enamour/frontend/src/server
+gunicorn --workers 1 --threads 12 -k gevent --bind 0.0.0.0:5000 server:app
