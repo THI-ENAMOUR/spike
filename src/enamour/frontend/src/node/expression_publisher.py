8e5914a84da4bc7e79f3329edfa3f9d3d159803a
mig3177 <mig3177@thi.de>
2022-04-25 18:53:19 +0200

    Web server and ROS node to display Spike's facial expression

---
+++ src/enamour/frontend/src/node/expression_publisher.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import requests
+
+# Publisher Message Format
+# '{"expression:" "<type>"}'
+# eg. with rostopic command:
+# pub rostopic pub /cmd_facial_expression std_msgs/String "data: '{\"expression\": \"default\"}'"
+
+URL = 'http://localhost:5000/update-facial-expression'
+HEADERS = {'content-type': 'application/json'}
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "publish %s", data.data)
+    # Post command to web server
+    try:
+        requests.post(URL,
+                      json=data.data, headers=HEADERS)
+    except Exception as exp:
+        print('Post to server unsuccessful', exp)
+
+
+def start_listener():
+
+    rospy.init_node('facial_expression_listener', anonymous=True)
+    # Get cmd from backend controller
+    rospy.Subscriber("cmd_facial_expression", String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    start_listener()
---
+++ src/enamour/frontend/src/server/server.py
@@ -0,0 +1,64 @@
+import flask
+import json
+from media_queue import MediaQueue
+
+
+app = flask.Flask(__name__, static_folder='static')
+
+media_Queue = MediaQueue()
+
+# Mapping between CMD and media files (face-expression.json)
+with open('face-expression.json') as expression_json:
+    registry = json.load(expression_json)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/favicon.ico', methods=['GET'])
+def fav():
+    return flask.url_for('static', filename='favicon.ico')
+
+# Set next facial expression
+@app.route('/update-facial-expression', methods=['POST'])
+def update():
+    try:
+        data = flask.request.get_json()
+        media = cmd_to_media(data)
+        media_Queue.publish(msg=media)
+        return {}, 200
+    except Exception as e:
+        print(e)
+        print('error')
+        return {}, 400
+
+# Get next facial expression
+@app.route('/next-facial-expression', methods=['GET'])
+def listen():
+
+    def stream():
+        messages = media_Queue.subscribe()  # returns a queue.Queue
+        while True:
+            msg = messages.get()  # blocks until a new message arrives
+            yield msg
+
+    return flask.Response(stream(), mimetype='text/event-stream')
+
+# Convert received command to media files
+def cmd_to_media(data):
+    dic = json.loads(data)
+    try:
+        obj = registry[dic['expression']]
+        json_str = json.dumps(obj)
+    except:
+        obj = registry['default']
+        json_str = json.dumps(obj)
+    return json_str
+
+# Start webserver
+# Requirements: 
+# flask, gunicorn, gevent
+# CMD:
+# gunicorn --workers 1 --threads 12 -k gevent --bind 0.0.0.0:5000 server:app
