a7881745c1fb5a16c09f22f2051744e625bc4b1a
Michael Angerer <mia6329@thi.de>
2022-04-25 14:00:31 +0200

    changed executable name

--- devel/share/gennodejs/ros/unitree_legged_msgs/msg/_index.js
+++ devel/share/gennodejs/ros/unitree_legged_msgs/msg/_index.js
@@ -1,24 +1,24 @@
 
 "use strict";
 
-let LowCmd = require('./LowCmd.js');
-let IMU = require('./IMU.js');
-let Cartesian = require('./Cartesian.js');
-let MotorState = require('./MotorState.js');
-let HighState = require('./HighState.js');
-let LED = require('./LED.js');
-let LowState = require('./LowState.js');
-let HighCmd = require('./HighCmd.js');
 let MotorCmd = require('./MotorCmd.js');
+let IMU = require('./IMU.js');
+let MotorState = require('./MotorState.js');
+let LowState = require('./LowState.js');
+let LowCmd = require('./LowCmd.js');
+let HighCmd = require('./HighCmd.js');
+let LED = require('./LED.js');
+let Cartesian = require('./Cartesian.js');
+let HighState = require('./HighState.js');
 
 module.exports = {
-  LowCmd: LowCmd,
-  IMU: IMU,
-  Cartesian: Cartesian,
-  MotorState: MotorState,
-  HighState: HighState,
-  LED: LED,
-  LowState: LowState,
-  HighCmd: HighCmd,
   MotorCmd: MotorCmd,
+  IMU: IMU,
+  MotorState: MotorState,
+  LowState: LowState,
+  LowCmd: LowCmd,
+  HighCmd: HighCmd,
+  LED: LED,
+  Cartesian: Cartesian,
+  HighState: HighState,
 };
