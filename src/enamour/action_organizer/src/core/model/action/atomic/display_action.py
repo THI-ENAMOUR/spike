02477016af9dab6fca739710cdc6c59ef2a1e361
Sebastian Fleig <sef7714@thi.de>
2022-04-26 20:38:22 +0200

    Basis für Controller und Actions erstellt

--- src/enamour/action_organizer/src/core/model/action/atomic/display_action.py
+++ src/enamour/action_organizer/src/core/model/action/atomic/display_action.py
@@ -1,6 +1,17 @@
+from src.controller import controller_provider
+from src.controller.controller import Controller
+from src.core.model.action.action_type import ActionType
 from src.core.model.action.atomic.generic.atomic_action import AtomicAction
+from src.core.model.action.execution_method import ExecutionMethod
+from src.core.model.action.selection_type import SelectionType, SelectAlways
 
 
 # TODO: Implement
 class DisplayAction(AtomicAction):
-    raise NotImplementedError
+    def __init__(self, selection_type: SelectionType = SelectAlways()):
+        super(DisplayAction, self).__init__(
+            action_type=ActionType.DISPLAY_ACTION, selection_type=selection_type, execution_method=ExecutionMethod.MULTIPLE
+        )
+
+    def get_controller(self) -> Controller:
+        return controller_provider.display_controller
--- src/enamour/action_organizer/src/core/model/action/atomic/walk_action.py
+++
@@ -1,6 +0,0 @@
-from src.core.model.action.atomic.generic.atomic_action import AtomicAction
-
-
-# TODO: Implement
-class WalkAction(AtomicAction):
-    raise NotImplementedError
