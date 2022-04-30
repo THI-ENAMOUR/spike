d9645dc78f1da599bfff52a255b71a98e7659985
sit7432 <sit7432@thi.de>
2022-04-30 04:05:56 +0200

    Adjust api server to meet newly definied api contract

--- src/enamour/api/api.py
+++ src/enamour/api/api.py
@@ -1,33 +1,31 @@
-from fastapi import FastAPI, Request, status
-from rospy_message_converter import json_message_converter
-import uvicorn
-from std_msgs.msg import String
+import json
+
 import rospy
+import uvicorn
+from fastapi import FastAPI, Request, status
+from starlette.responses import Response
+from std_msgs.msg import String
 
 API_HOST = "127.0.0.1"
 API_PORT = 5000
 
 app = FastAPI()
-pub = rospy.Publisher('chatter', String, queue_size=10)
-rospy.init_node('talker', anonymous=True)
+pub = rospy.Publisher("intent", String, queue_size=10)
+rospy.init_node("intent_api_service")
 
 
-@app.post("/set-ki-intent", status_code=status.HTTP_204_NO_CONTENT)
-async def update(request: Request):
+@app.post("/intent", status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+async def receive_intent(request: Request):
     data = await request.json()
-    publish(data)
-    return
+    publish_intent(data)
 
 
-def publish(message):
-    data = json_message_converter.convert_json_to_ros_message('std_msgs/String', message)
-    pub.publish(data)
-    return None
+def publish_intent(intent):
+    json_string = json.dumps(intent)
+    pub.publish(json_string)
 
 
 if __name__ == "__main__":
     uvicorn.run("api:app", host=API_HOST, port=API_PORT, log_level="info")
 
-
-
-# curl -X POST http://localhost:5000/set-ki-intent -H "Content-Type: application/json" -d '{"message":"Hello"}'
+# curl -X POST http://localhost:5000/intent -H "Content-Type: application/json" -d '{"message":"Hello"}'
