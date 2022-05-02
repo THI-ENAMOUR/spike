ace370de843104aeec207577a96c114ca679770f
sef7714 <sef7714@thi.de>
2022-05-02 18:26:20 +0200

    changed logger class

--- src/enamour/action_organizer/src/util/logger.py
+++ src/enamour/action_organizer/src/util/logger.py
@@ -1,21 +1,32 @@
-import rospy
+import logging
 
 
 class Logger:
+    """Provides functions to print logs to the console and log files (log files are located in spike/logs"""
+
     def __init__(self, name):
         self.name = name
+        self.logger = logging.getLogger(name)
+        self.c_handler = logging.StreamHandler()
+        self.f_handler = logging.FileHandler("../../../logs/" + self.name + ".log")
+
+        self.logger.setLevel(logging.DEBUG)
+        self.c_handler.setLevel(logging.DEBUG)
+        self.f_handler.setLevel(logging.ERROR)
+        self.logger.addHandler(self.c_handler)
+        self.logger.addHandler(self.f_handler)
 
     def info(self, message):
-        rospy.loginfo(f"\n[{self.name}]: \n{message}")
+        self.logger.info(f"INFO - [{self.name}]: \n{message}")
 
     def warning(self, message):
-        rospy.logwarn(f"\n[{self.name}]: \n{message}")
+        self.logger.warning(f"WARNING - [{self.name}]: \n{message}")
 
     def error(self, message):
-        rospy.logerr(f"\n[{self.name}]: \n{message}")
-
-    def fatal(self, message):
-        rospy.logfatal(f"\n[{self.name}]: \n{message}")
+        self.logger.error(f"ERROR - [{self.name}]: \n{message}")
 
     def debug(self, message):
-        rospy.logdebug(f"\n[{self.name}]: \n{message}")
+        self.logger.debug(f"DEBUG - [{self.name}]: \n{message}")
+
+    def critical(self, message):
+        self.logger.critical(f"CRITICAL - [{self.name}]: \n{message}")
