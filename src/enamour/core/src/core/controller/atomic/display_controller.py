f7ec8bac7ebaf08c709780ab92ac15f2a3ccf908
ibd4301 <ibd4301@thi.de>
2022-05-17 00:03:47 +0200

    sound and DisplayController implemented

--- src/enamour/core/src/core/controller/atomic/display_controller.py
+++ src/enamour/core/src/core/controller/atomic/display_controller.py
@@ -1,8 +1,21 @@
+import rospy
+from std_msgs.msg import String
+
 from core.controller.controller import Controller
+from core.model.action.atomic.display_action import DisplayAction
+from error.illegal_state_error import IllegalStateError
+from util.logger import Logger
 
 
 class DisplayController(Controller):
     """Controller for Actions regarding the display."""
 
+    __logger = Logger(__name__)
+    publisher = rospy.Publisher("/display", String, queue_size=10)
+
     def execute_action(self, action):
-        raise NotImplementedError
+        if not isinstance(action, DisplayAction):
+            raise IllegalStateError("This controller does not support the action " + str(action))
+        DisplayController.__logger.debug("executing display action " + str(action))
+        DisplayController.publisher.publish(action.name)
+        action.complete()
--- src/enamour/core/src/core/model/action/atomic/display_action.py
+++ src/enamour/core/src/core/model/action/atomic/display_action.py
@@ -5,13 +5,14 @@ from core.model.action.timing_option import StartTime
 
 
 class DisplayAction(AtomicAction):
-    def __init__(self, start_ms, timing_option=None):
+    def __init__(self, start_ms, display_name, timing_option=None):
         timing_option = timing_option if timing_option is not None else StartTime(start_ms=start_ms)
         super(DisplayAction, self).__init__(
             action_type=ActionType.DISPLAY_ACTION,
             timing_option=timing_option,
             execution_method=ExecutionMethod.MULTIPLE,
         )
+        self.name = display_name
 
     def get_controller(self):
         # Local import to break cyclic import chain
