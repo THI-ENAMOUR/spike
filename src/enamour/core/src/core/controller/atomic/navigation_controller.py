3e7f0ef8fd260919b6627221c7492fdc3e9e0285
sef7714 <sef7714@thi.de>
2022-05-23 23:36:12 +0200

    started with the navigation_controller, attributes were added to navigation_action

--- src/enamour/core/src/core/controller/atomic/navigation_controller.py
+++ src/enamour/core/src/core/controller/atomic/navigation_controller.py
@@ -1,8 +1,50 @@
 from core.controller.controller import Controller
+from core.model.action.atomic.navigation_action import NavigationAction
+from core.model.action.timing_option import Duration, StartTime
+from error.illegal_state_error import IllegalStateError
+from util.logger import Logger
+import rospy
+from geometry_msgs.msg import Twist
 
 
 class NavigationController(Controller):
     """Controller for making the robot walk to the provided position."""
 
     def execute_action(self, action):
-        raise NotImplementedError
+        
+        if not isinstance(action, NavigationAction):
+            raise IllegalStateError("This controller does not support the action " + str(action))
+
+        logger = Logger("cmd_vel")
+        velocity_publisher = rospy.Publisher('cmd_vel', log_level=rospy.DEBUG)
+        rate = rospy.Rate(1000)
+
+        # For cmd_vel messages have to be of type Twist, Twist will have the attributes of the provided action
+        vel_msg = Twist()
+        vel_msg.linear.x = action.linear.x
+        vel_msg.linear.y = action.linear.y
+        vel_msg.linear.z = action.linear.z
+        vel_msg.angular.x = action.angular.x
+        vel_msg.angular.y = action.angular.y
+        vel_msg.angular.z = action.angular.z
+
+        if action.timing_option == StartTime:
+            # Publishes messages in the topic after the start time was hit
+            while not rospy.is_shutdown() and action.timing_option.in_time_frame():
+                velocity_publisher.publish(vel_msg)
+                logger.info(vel_msg)
+                rate.sleep()
+
+        elif action.timing_option == Duration:
+            # Publishes messages in the topic while in the given duration
+            while not rospy.is_shutdown and action.timing_option.in_time_frame():
+                velocity_publisher.publish(vel_msg)
+                logger.info(vel_msg)
+                rate.sleep()
+
+        else:
+            raise NotImplementedError(
+                "Timing option {timing} for action {id} not implemented".format(
+                    timing=action.timing_option, id=action.id
+                )
+            )
--- src/enamour/core/src/core/model/action/atomic/navigation_action.py
+++ src/enamour/core/src/core/model/action/atomic/navigation_action.py
@@ -1,17 +1,29 @@
 from core.model.action.action_type import ActionType
 from core.model.action.atomic.atomic_action import AtomicAction
 from core.model.action.execution_method import ExecutionMethod
-from core.model.action.timing_option import StartTime
+from core.model.action.timing_option import StartTime, Duration
+from core.model.common.vector3 import Vector3
 
 
 class NavigationAction(AtomicAction):
-    def __init__(self, start_ms, timing_option=None):
-        timing_option = timing_option if timing_option is not None else StartTime(start_ms=start_ms)
+    def __init__(self, start_ms, end_ms, timing_option=None, x=0, y=0, z=0, ax=0, ay=0, az=0):
+
+        if timing_option is None:
+            if end_ms is None:
+                timing_option = StartTime(start_ms=start_ms)
+            else:
+                timing_option = Duration(start_ms=start_ms, end_ms=end_ms)
+
+        #Zeile darunter ist einfach mal auskommentiert, falls man die wieder brauchen würde (ich denke mal, dass wir eien Duration brauchen werden, wenn wir cmd_vel verwenden)
+        #timing_option = timing_option if timing_option is not None else StartTime(start_ms=start_ms) 
         super(NavigationAction, self).__init__(
             action_type=ActionType.BODY_MOVEMENT_ACTION,
             timing_option=timing_option,
             execution_method=ExecutionMethod.NO_SAME_TYPE,
         )
+
+        self.linear = Vector3(x, y, z)
+        self.angular = Vector3(ax, ay, az)
 
     def get_controller(self):
         # Local import to break cyclic import chain
