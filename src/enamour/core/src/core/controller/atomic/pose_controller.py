050e2e2423f81c7d2e2703870925992b3c21f8f5
ibd4301 <ibd4301@thi.de>
2022-05-24 23:00:19 +0200

    pose-controller first implemention

--- src/enamour/core/src/core/controller/atomic/pose_controller.py
+++ src/enamour/core/src/core/controller/atomic/pose_controller.py
@@ -8,12 +8,31 @@ from unitree_legged_msgs.msg import HighCmd
 
 class PoseController(Controller):
     """Controller for moving the robot body in the provided position."""
-    a_measure_roll = 0                                                       #globale Variable
-    a_measure_pitch = 0                                                      #globale Variable
-    a_measure_yaw = 0                                                        #globale Variable
-    t_ak_roll = 0                                                          #globale Variable
-    t_ak_pitch = 0                                                           #globale Variable
-    t_ak_yaw = 0
+
+    # note: 'a_' := angle
+    #       't_' := y-intercept
+    #       'm_' := slope
+
+    # temporary without using highState
+
+    time_prev = -1
+
+    a_last_roll = 0
+    a_last_pitch = 0
+    a_last_yaw = 0
+
+    t_roll = 0
+    t_pitch = 0
+    t_yaw = 0
+
+    m_roll = 0
+    m_pitch = 0
+    m_yaw = 0
+
+    def getCurrentHighState(self):
+        # TODO
+        return {'roll': self.a_last_row, 'pitch': self.a_last_pitch, 'yaw': self.a_last_yaw}
+
     def execute_action(self, action):
 
         if not isinstance(action, PoseAction):
@@ -24,63 +43,63 @@ class PoseController(Controller):
             pass
         elif action.timing_option == Duration:
 
-            a_mea_roll = action.linear.x              # Aufruf der measurement fuer ROLL
-            a_mea_pitch = action.linear.z            # Aufruf der measurement fuer PITCH
-            a_mea_yaw = action.linear.y                 # Aufruf der measurement fuer YAW
+            a_goal_roll = action.linear.ax  # fuellen der Variable mit der Ziel Kordinate
+            a_goal_pitch = action.linear.az  # fuellen der Variable mit der Ziel Kordinate
+            a_goal_yaw = action.linear.ay  # fuellen der Variable mit der Ziel Kordinate
 
-            a_goal_roll = action.linear.ax                   # fuellen der Variable mit der Ziel Kordinate
-            a_goal_pitch = action.linear.az                  # fuellen der Variable mit der Ziel Kordinate
-            a_goal_yaw = action.linear.ay                    # fuellen der Variable mit der Ziel Kordinate
+            a_measured_roll = self.getCurrentHighState()['roll']
+            a_measured_pitch = self.getCurrentHighState()['pitch']
+            a_measured_yaw = self.getCurrentHighState()['yaw']
 
-            t_ak_roll = self.t_ak_roll                      # Aufruf der y Achsenschnittstelle  für ROLL
-            t_ak_pitch = self.t_ak_pitch                    # Aufruf der y Achsenschnittstelle  für PITCH
-            t_ak_yaw = self.t_ak_yaw                        # Aufruf der y Achsenschnittstelle  für YAW
+            if not (action.get_parent_time() >= action.timing_option.end_time or a_measured_roll >= a_goal_roll and
+                    a_measured_yaw >= a_goal_yaw and a_measured_pitch >= a_goal_pitch):
+                if self.time_prev == -1:
+                    # 1st iteration -> set up the first linear equations, do not publish
+                    self.m_pitch = (a_goal_pitch - a_measured_pitch) /\
+                                   (action.timing_option.end_time - action.get_parent_time())
+                    self.t_pitch = a_measured_pitch - (self.m_pitch * action.get_parent_time())
 
-            t_ak_roll = action.linear.x                     #befuellen des y Achsenabschnittes mit ihrem Wert
-            t_ak_pitch = action.linear.z                    #befuellen des y Achsenabschnittes mit ihrem Wert
-            t_ak_yaw = action.linear.y                      #befuellen des y Achsenabschnittes mit ihrem Wert
+                    self.m_roll = (a_goal_roll - a_measured_roll) /\
+                                  (action.timing_option.end_time - action.get_parent_time())
+                    self.t_roll = a_measured_roll - (self.m_roll * action.get_parent_time())
 
-            m_acc_roll = a_mea_roll                         # füllen der zu benutzenden Variablen
-            m_acc_pitch = a_mea_pitch                       # füllen der zu benutzenden Variablen
-            m_acc_yaw = a_mea_yaw                           # füllen der zu benutzenden Variablen
+                    self.m_yaw = (a_goal_yaw - a_measured_yaw) /\
+                                 (action.timing_option.end_time - action.get_parent_time())
+                    self.t_yaw = a_measured_yaw - (self.m_yaw * action.get_parent_time())
+                else:
+                    # 1. correct slope and y-intercept from previous tick with a_measured_*
+                    self.m_pitch = (a_goal_pitch - a_measured_pitch) / (action.timing_option.end_time - self.time_prev)
+                    self.t_pitch = a_measured_pitch - (self.m_pitch * self.time_prev)
 
-            publish_roll = 0                                # Initialisierung für die erste Runde
-            publish_pitch = 0                               # Initialisierung für die erste Runde
-            publish_yaw = 0                                 # Initialisierung für die erste Runde
+                    self.m_roll = (a_goal_roll - a_measured_roll) / (action.timing_option.end_time - self.time_prev)
+                    self.t_roll = a_measured_roll - (self.m_roll * self.time_prev)
 
+                    self.m_yaw = (a_goal_yaw - a_measured_yaw) / (action.timing_option.end_time - self.time_prev)
+                    self.t_yaw = a_measured_yaw - (self.m_yaw * self.time_prev)
 
+                    # 2. calculate angle of current time with corrected linear equation
+                    publish_pitch = self.m_pitch * action.get_parent_time() + self.t_pitch
+                    publish_roll = self.m_roll * action.get_parent_time() + self.t_roll
+                    publish_yaw = self.m_yaw * action.get_parent_time() + self.t_yaw
 
-        while not (action.get_parent_time() >= action.timing_option.end_time | a_mea_roll >= a_goal_roll | a_mea_yaw >= a_goal_yaw | a_mea_pitch >= a_goal_pitch):   # wenn gewuenschter Wert oder Zeit ereicht ist
+                    # 3. publish highCmd
+                    high_cmd = HighCmd()  # erstellen der Nachricht
+                    high_cmd_publisher = rospy.Publisher('high_command', HighCmd,
+                                                         queue_size=10)  # publisher für HighCmd Nachricht
+                    high_cmd.roll = publish_roll  # befuellen der Nachricht mit Werten
+                    high_cmd.pitch = publish_pitch  # befuellen der Nachricht mit Werten
+                    high_cmd.yaw = publish_yaw  # befuellen der Nachricht mit Werten
+                    high_cmd_publisher.publish(high_cmd)  # publishen der Nachricht
 
-            m_acc_roll = a_mea_roll                                                                                 # Updaten für die Naechsten Runden                                                                                    beenden der while schleife
-            m_acc_pitch = a_mea_pitch                                                                               # Updaten für die Naechsten Runden
-            m_acc_yaw = a_mea_yaw                                                                                   # Updaten für die Naechsten Runden
+                    # 3.1 TODO Temporary since we do not have the highStates yet
+                    self.a_last_pitch = publish_pitch
+                    self.a_last_roll = publish_roll
+                    self.a_last_yaw = publish_yaw
 
-            m_acc_roll = (a_goal_roll - a_mea_roll)/(action.timing_option.end_time - action.get_parent_time())      # Steigung der Aktuelen ROLL Graphen
-            publish_roll = m_acc_roll * action.timing_option + t_ak_roll                                            # Speicherung der Berechneten ROLL Werte(y wert)
-
-            m_acc_pitch = (a_goal_pitch - a_mea_pitch)/(action.timing_option.end_time - action.get_parent_time())    # Steigung der Aktuelen PITCH Graphen
-            publish_pitch = m_acc_pitch * action.timing_option + t_ak_pitch                                          # Speicherung der Berechneten PITCH Werte (y Wert)
-
-            m_acc_yaw = (a_goal_yaw - a_mea_yaw)/(action.timing_option.end_time - action.get_parent_time())         # Steigung der Aktuelen YAW Graphen
-            publish_yaw = m_acc_yaw * action.timing_option + t_ak_yaw                                               # Speicherung der Berechneten YAW Werte(y Wert)
-
-            high_cmd = HighCmd()                                                                                    # erstellen der Nachricht
-            high_cmd_publisher = rospy.Publisher('high_command', HighCmd, queue_size=10)                            # publisher für HighCmd Nachricht
-            high_cmd.roll = publish_roll                                                                            # befuellen der Nachricht mit Werten
-            high_cmd.pitch = publish_pitch                                                                          # befuellen der Nachricht mit Werten
-            high_cmd.yaw = publish_yaw                                                                              # befuellen der Nachricht mit Werten
-            high_cmd_publisher.publish(high_cmd)                                                                    # publishen der Nachricht
-
-            t_ak_roll = publish_roll - (m_acc_roll * action.timing_option)                                          # updaten der t Variable durch die Messung der echten Messwerte für ROLL
-            t_ak_pitch = publish_pitch - (m_acc_pitch * action.timing_option)                                       # updaten der t Variable durch die Messung der echten Messwerte für PITCH
-            t_ak_yaw = publish_yaw - (m_acc_yaw * action.timing_option)                                             # updaten der t Variable durch die Messung der echten Messwerte für YAW
-
-        action.completed()                                                                                          # Wenn abbruch Bedingung von der weil Schleife eintritt action.completed()
-
-
-
-           # # TODO Interpolate the pose based on the parent_duration, current location (TODO), and desired end position
+                # 4. update previous tick
+                self.time_prev = action.get_parent_time()
+            else:
+                action.completed()  # Wenn abbruch Bedingung von der weil Schleife eintritt action.completed()
             pass
         else:
             raise NotImplementedError(
