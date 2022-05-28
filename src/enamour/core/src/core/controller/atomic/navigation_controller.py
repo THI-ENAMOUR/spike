ab1859d1541eb147eef3da8805e3480bb500426d
mig3177 <mig3177@thi.de>
2022-05-28 22:49:31 +0200

    Add mapping from velCmd to HighCmd

--- src/enamour/core/src/core/controller/atomic/navigation_controller.py
+++ src/enamour/core/src/core/controller/atomic/navigation_controller.py
@@ -5,7 +5,7 @@ from error.illegal_state_error import IllegalStateError
 from util.logger import Logger
 import rospy
 from geometry_msgs.msg import Twist
-
+from unitree_legged_msgs.msg import HighCmd
 
 class NavigationController(Controller):
     """Controller for making the robot walk to the provided position."""
@@ -16,7 +16,7 @@ class NavigationController(Controller):
             raise IllegalStateError("This controller does not support the action " + str(action))
 
         logger = Logger("cmd_vel")
-        velocity_publisher = rospy.Publisher('cmd_vel', log_level=rospy.DEBUG)
+        velocity_publisher = rospy.Publisher('/high_command', HighCmd, queue_size=10)
         rate = rospy.Rate(1000)
 
         # For cmd_vel messages have to be of type Twist, Twist will have the attributes of the provided action
@@ -27,18 +27,21 @@ class NavigationController(Controller):
         vel_msg.angular.x = action.angular.x
         vel_msg.angular.y = action.angular.y
         vel_msg.angular.z = action.angular.z
+        highCmd = self.velCmdToHighCmd(vel_msg)
 
         if action.timing_option == StartTime:
             # Publishes messages in the topic after the start time was hit
             while not rospy.is_shutdown() and action.timing_option.in_time_frame():
-                velocity_publisher.publish(vel_msg)
+                velocity_publisher.publish(highCmd)
+                logger.info(vel_msg)
                 logger.info(vel_msg)
                 rate.sleep()
 
         elif action.timing_option == Duration:
             # Publishes messages in the topic while in the given duration
-            while not rospy.is_shutdown and action.timing_option.in_time_frame():
-                velocity_publisher.publish(vel_msg)
+            while not rospy.is_shutdown() and action.timing_option.in_time_frame():
+                velocity_publisher.publish(highCmd)
+                logger.info(vel_msg)
                 logger.info(vel_msg)
                 rate.sleep()
 
@@ -48,3 +51,45 @@ class NavigationController(Controller):
                     timing=action.timing_option, id=action.id
                 )
             )
+        
+    # Convert velocity to proportional HighCmd value
+    def propToHighCMD(self, max_back_right_speed, max_forward_left_speed, value):
+        newValue = 0.0
+        if (value < 0):
+            newValue = value / max_back_right_speed
+            if (newValue < -1):
+                # Set max backward or right speed
+                newValue = -1.0
+        else:
+            newValue =  value / max_forward_left_speed
+            if (newValue > 1):
+                # Set max forward or left speed
+                newValue = 1.0
+
+        return newValue
+      
+    # Convert velocity command (Twist message) to Unitree HighCMD
+    def velCmdToHighCmd(self, twist):
+
+        # HighCmd value speed between -1 and 1. Value corresponds to a linear proportional 
+        # value of -0.7 m/s (max backward speed) and 1 m/s (max forward speed)
+        forwardSpeed = self.propToHighCMD(0.7, 1.0, twist.linear.x) 
+
+
+        # HighCmd value speed between -1 and 1. Value corresponds to a linear proportional 
+        # value of -0.4 m/s (max rightward speed) and 0.4 m/s (max leftward speed)
+        sideSpeed = self.propToHighCMD(0.4,0.4,twist.linear.y)
+        
+        # HighCmd value speed between -1 and 1. Value corresponds to a linear proportional 
+        # value of -120 deg/s / -2.0944 rad/s (max rightward speed) and 
+        # 120 deg/s / 2.0944 rad/s (max leftward speed)
+        rotateSpeed = self.propToHighCMD(2.0944, 2.0944, twist.angular.z)
+
+        highCmd = HighCmd()
+        highCmd.levelFlag = 0x00;   # HIGHLEVEL 
+        highCmd.mode = 2            # Walking mode
+        highCmd.forwardSpeed = forwardSpeed
+        highCmd.sideSpeed = sideSpeed
+        highCmd.rotateSpeed = rotateSpeed
+
+        return highCmd
